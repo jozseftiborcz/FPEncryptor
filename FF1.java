1ab582c36a5a0aa283d31f73a734b5580b77bae6
Tiborcz József <jozsef.tiborcz@gmail.com>
2015-11-18 06:41:18 +0100

    PoC: encryption and decryption implemented

--- FF1.java
+++ FF1.java
@@ -7,6 +7,7 @@ import java.security.SecureRandom;
 
 public class FF1 {
 
+    final public static boolean debug = false;
     final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
     public static String bytesToHex(byte[] bytes) {
@@ -28,21 +29,20 @@ public class FF1 {
         return Long.parseLong(input, 10);
     }
 
-    public static String num2IntStr(long num) {
-        return Long.toString(num, 10);
+    public static String num2IntStr(long num, int length) {
+        String s = Long.toString(num, 10);
+        while (s.length() < length) s = "0"+s;
+        return s;
     }
 
     // based on NIST SP800 38g, FF2
-    public static String encryptCCN(SecretKey key, IvParameterSpec iv, String tweak, String input) throws Exception {
+    public static String encryptCCN(SecretKey key, String tweak, String input) throws Exception {
         long ccn = intStr2Num(input);
-        System.out.println(ccn);
-        int A = (int) (ccn >>> 10) & 0x03ff; // 10 bits
-//        System.out.println("A"+A);
-        int B = (int) ccn & 0x03ff;
-//        System.out.println("B"+B);
+        int A = (int) ccn>>10;
+        int B = (int) ccn&0x3ff;
         byte radix = 0x0a;
         Cipher aes = Cipher.getInstance("AES/ECB/NoPadding");
-        aes.init(Cipher.ENCRYPT_MODE, key, iv);
+        aes.init(Cipher.ENCRYPT_MODE, key); //, iv);
         byte[] P = new byte[16];
         if (tweak.length()>0) {
             P[0] = radix;
@@ -57,28 +57,65 @@ public class FF1 {
             P[15] = (byte) (twn & 0xff);
         }
         byte[] encryptionKey = aes.doFinal(P);
-        aes.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptionKey, "AES"), iv);
+        aes.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptionKey, "AES")); //, iv);
         for (int i=0; i<10; i++) {
+            int a = A;
+            int b = B;
+            byte[] Q = new byte[16];
+            Q[0] = (byte) i;
+            Q[14] = (byte) ((B >> 8) & 0xff); // two bytes are enough
+            Q[15] = (byte) (B & 0xff);
+            byte[] Y = aes.doFinal(Q);
+            int y = (((int)Y[14]&0x03)<<8)+((int)Y[15]&0xff);
+            if (debug) System.out.println(bytesToHex(Q) + " -> y" + y);
+            int C = A^y;
+            A = B;
+            B = C;
+            if (debug) System.out.println("encr: A " +a+ " B " + b + " -> " + "A " +A+ " B " + B);
+        }
+        String eccn = num2IntStr((A<<10)+B,6);
+        if (eccn.length()>6) return encryptCCN(key, tweak, eccn); else return eccn;
+    }
+
+    public static String decryptCCN(SecretKey key, String tweak, String input) throws Exception {
+        long ccn = intStr2Num(input);
+        int A = (int) ccn>>10;
+        int B = (int) ccn&0x3ff;
+        byte radix = 0x0a;
+        Cipher aes = Cipher.getInstance("AES/ECB/NoPadding");
+        aes.init(Cipher.ENCRYPT_MODE, key); //, iv);
+        byte[] P = new byte[16];
+        if (tweak.length()>0) {
+            P[0] = radix;
+            P[1] = (byte) (tweak.length() & 0xff);
+            P[2] = 0x06; // length of CCN middle part to encrypt
+            for(int i=3;i<11;i++) P[i] = 0;
+            long twn = Long.parseLong(tweak, 10); // should fit within 5 bytes
+            P[11] = (byte) (twn >> 32 & 0xff);
+            P[12] = (byte) (twn >> 24 & 0xff);
+            P[13] = (byte) (twn >> 16 & 0xff);
+            P[14] = (byte) (twn >> 8 & 0xff);
+            P[15] = (byte) (twn & 0xff);
+        }
+        byte[] encryptionKey = aes.doFinal(P);
+        aes.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptionKey, "AES")); //, iv);
+        for (int i=9; i>=0; i--) {
+            int a = A;
+            int b = B;
             byte[] Q = new byte[16];
             Q[0] = (byte) i;
             Q[14] = (byte) ((A >> 8) & 0xff); // two bytes are enough
             Q[15] = (byte) (A & 0xff);
-            //System.out.println(bytesToHex(Q));
             byte[] Y = aes.doFinal(Q);
-            //System.out.println(bytesToHex(Y));
-            int y = ((int)Y[14]&0x03)*256+((int)Y[15]&0xff);
-            //System.out.println("y"+y);
-            int C = (A+y) % 1024;
-            //System.out.println("A"+A);
-            //System.out.println("B"+B);
-            //System.out.println("C"+C);
-            A = B;
-            B = C;
+            int y = (((int)Y[14]&0x03)<<8)+((int)Y[15]&0xff);
+            if (debug) System.out.println(bytesToHex(Q) + " -> y" + y);
+            int C = B^y;
+            B = A;
+            A = C;
+            if (debug) System.out.println("decr: A " +a+ " B " + b + " -> " + "A " +A+ " B " + B);
         }
-            //System.out.println("A"+A);
-            //System.out.println("B"+B);
-        //System.out.println("AB"+((A<<10)+B));
-        return num2IntStr(((A<<10)+B) % 1048576); // 2^20
+        String eccn = num2IntStr((A<<10)+B,6);
+        if (eccn.length()>6) return decryptCCN(key, tweak, eccn); else return eccn;
     }
 
     public static void main(String[] args) throws Exception {
@@ -91,10 +128,22 @@ public class FF1 {
         byte iv[] = new byte[16];
         sr.nextBytes(iv);
         IvParameterSpec ivs = new IvParameterSpec(iv);
-        System.out.println(encryptCCN(key, ivs, "1234561234", "123456"));
+//        System.out.println(encryptCCN(key, ivs, "1234561234", "123456"));
+        for (String ccn : new String[]{"1234561234561234", "1234562345611234", "2234561234561234", "6543211234564321", "4321654321654321"}) {
+            String tweak = ccn.substring(0,6)+ccn.substring(12);
+            String pan = ccn.substring(6,12); 
+            String epan = encryptCCN(key, tweak, pan);
+            System.out.println(pan + "->" + epan + "("+tweak+")->" + decryptCCN(key, tweak, epan));
+        }
+        /*
         System.out.println(encryptCCN(key, ivs, "2234561234", "123456"));
+        System.out.println(decryptCCN(key, ivs, "2234561234", encryptCCN(key, ivs, "2234561234", "123456")));
         System.out.println(encryptCCN(key, ivs, "1234561234", "223456"));
+        System.out.println(decryptCCN(key, ivs, "1234561234", encryptCCN(key, ivs, "1234561234", "223456")));
         System.out.println(encryptCCN(key, ivs, "1234561234", "323456"));
+        System.out.println(decryptCCN(key, ivs, "1234561234", encryptCCN(key, ivs, "1234561234", "323456")));
         System.out.println(encryptCCN(key, ivs, "1234561234", "523456"));
+        System.out.println(decryptCCN(key, ivs, "1234561234", encryptCCN(key, ivs, "1234561234", "523456")));
+        */
     }
 }
